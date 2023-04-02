0e6b0c1068f9fda90e5b6617b588a4a02478c138
Aditya Dhage <adityadhage66927@gmail.com>
2023-04-02 15:41:59 +0530

    Day 2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  //saara ui ka kam build method ke andar hota hai ;build ko bar bar call krna hota hai coz ui ko refresh krta hai
+
+  final int days = 30;
+  final String name = "Aditya";
+  String surname = "Dhage";
+  // double num = 40.2323;
+  //  no float, long short byte
+  // bool isMale = true;
+  // num temp = 30.5; //takes both int and float
+
+  // var day = "Tuesday"; datatype decided by compiler
+  // const pi = 3.14;
+  // final variable similar to const but in final for eg: if there is a list then sum can be done , but not in const
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // It is a widget ; component of material ; contains head(appBar) body foot((Drawer,Bottom Navigation bar
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of Flutter by $name " +
+              surname), //$ string interpolation
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,10 @@ void main() {
 
 //Hot reload : small change Hot Restart : app related change
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    //saara ui ka kam build method ke andar hota hai ;build ko bar bar call krna hota hai coz ui ko refresh krta hai
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of Flutter"),
-          ),
-        ),
-      ),
-    );
+      home: Homepage,
+    ); 
   }
 }
