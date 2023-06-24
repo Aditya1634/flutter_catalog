09d7236d3ecadef23bb46e916a0254ccf1ff9e3a
Aditya Dhage <adityadhage66927@gmail.com>
2023-06-24 17:38:01 +0530

    Day 4 Completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,17 +1,59 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold
-        ),),
-        ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ), //for spacing instead of padding
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ), //To give space like padding
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: " Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true, //to make password dot dot sot
+                    decoration: InputDecoration(
+                      hintText: " Enter Password",
+                      labelText: "Password ",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("Hi Aditya");
+                      },
+                      child: Text("Login"))
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/Pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'Pages/home_page.dart';
 
 void main() {
@@ -8,28 +9,29 @@ void main() {
 
 //Hot reload : small change Hot Restart : app related change
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       //home: HomePage(), commented because it is in default route HomePage() cant occur twice.
       themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple //Adjusts all colors according to Purple(here).
-      ),
+          primarySwatch:
+              Colors.deepPurple, //Adjusts all colors according to Purple(here).
+              fontFamily: GoogleFonts.lato().fontFamily,      
+          ),
       darkTheme: ThemeData(
-        brightness: Brightness.dark, //  Brightness.dark overrides theme so colors of primarySwatch wont work in dark mode
-      //primarySwatch: Colors.red
+        brightness: Brightness
+            .dark, //  Brightness.dark overrides theme so colors of primarySwatch wont work in dark mode
+        //primarySwatch: Colors.red
       ),
       // routes are ways for an app boht sare raaste ho skte to go in app/navigate
       //it takes a map(key,value)
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
         "/": (context) => LoginPage(), //default route "/" means HomePage
-        "/home":(context) =>  HomePage(),// they are objects of classes therefore new keyword is working
-        "/login" :(context) => LoginPage()
-      }, 
-    ); 
+        "/home": (context) => HomePage(), // they are objects of classes therefore new keyword is working
+        "/login": (context) => LoginPage(),
+      },
+    );
   }
 }
-
