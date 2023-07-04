e1dc9915d1f890f9549c550e18680fe7f2f4116e
Aditya Dhage <adityadhage66927@gmail.com>
2023-07-04 09:54:40 +0530

    Day 6 Completed with Animated Container

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+//There are two classes in stateful widget
+
+class _LoginPageState extends State<LoginPage> {
+  // _ makes variables, methods, classes private
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,7 +28,7 @@ class LoginPage extends StatelessWidget {
                 height: 20.0,
               ), //for spacing instead of padding
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
@@ -33,11 +43,15 @@ class LoginPage extends StatelessWidget {
                 child: Column(
                   children: [
                     TextFormField(
-                      decoration: InputDecoration(
-                        hintText: " Enter Username",
-                        labelText: "Username",
-                      ),
-                    ),
+                        decoration: InputDecoration(
+                          hintText: " Enter Username",
+                          labelText: "Username",
+                        ),
+                        onChanged: ((value) {
+                          name = value;
+                          // build method ko call dena padega to reflect changes kyuki ui usme hi likha hai pura but direct call nahi de skte like build() isse code fat jayega..... uske liye method  hai setState
+                          setState(() {}); // Calls build method
+                        })),
                     TextFormField(
                       obscureText: true, //to make password dot dot sot
                       decoration: InputDecoration(
@@ -48,23 +62,57 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 40.0,
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        //In built class in material
-                        //Methods: push : ek class aage jana
-                        //pop: ek class peeche jana
-                        //push me contex bhi hona 
-                        //pushNamed me buildContext se by default context le leta
+
+                    // TO make a widget clickable: two methods
+                    // 1. Wrap contaiber with inkwell (Splash/Ripple colour)
+                    // 2. Wrap with Gesture Detector (No Feedback)
+                    InkWell(
+                      //for await method should bve async
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                    )
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 50 : 150,
+                        height: 50,
+                        //color: Colors.deepPurple,
+                        alignment: Alignment.center,
+                        child: changeButton?Icon(Icons.done,color: Colors.white,) :Text(
+                          "Login",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            // shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(changeButton?50:8)),
+                      )
+                      ),
+                    
+
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     //In built class in material
+                    //     //Methods: push : ek class aage jana
+                    //     //pop: ek class peeche jana
+                    //     //push me contex bhi hona
+                    //     //pushNamed me buildContext se by default context le leta
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   child: Text("Login"),
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    // )
                   ],
                 ),
               )
             ],
           ),
-        ));
+        )); //TO prevent overflow
   }
 }
